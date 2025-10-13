6ac1015bd3f6d1112ee3e7c370a4ba773525fa46
Xavier <xavieralves162@gmail.com>
2025-10-13 13:55:44 +0100

    Added post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+from django.conf import settings
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content[:30]}"
