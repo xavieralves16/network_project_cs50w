bcba49d46a16e59d40bbde3f8b0f42a21fb29ea9
Xavier <xavieralves162@gmail.com>
2025-10-13 14:50:03 +0100

    Bug fixes in like

--- network/views.py
+++ network/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import User, Post
 
@@ -19,7 +20,16 @@ def index(request):
 
 
 
+@login_required
+def like_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
 
+    if request.method == "POST":
+        if request.user in post.likes.all():
+            post.likes.remove(request.user)  # já deu like → unlike
+        else:
+            post.likes.add(request.user)  # ainda não → like
+    return redirect("index")
 
 def login_view(request):
     if request.method == "POST":
