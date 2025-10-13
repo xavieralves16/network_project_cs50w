fb33003936b4fe9837f5c9512c5124a2828dc446
Xavier <xavieralves162@gmail.com>
2025-10-13 15:15:21 +0100

    Added follow model

--- network/models.py
+++ network/models.py
@@ -24,3 +24,19 @@ class Post(models.Model):
     @property
     def like_count(self):
         return self.likes.count()
+    
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followers"
+    )
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
